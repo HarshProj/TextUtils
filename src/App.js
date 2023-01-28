f41e87df7dfba57cf8a54dcebf7c6f1cc1a80e1c
Harsh Chauhan <hharshchauhan1@gmail.com>
2023-01-28 17:23:57 +0530

    thig

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import { useState } from 'react';
 import './App.css';
 import Alert from './components/Alert';
-import About from './components/About';
+// import About from './components/About';
 import Navbar from './components/Navbar';
 import TextForm from './components/TextForm';
 // import{
